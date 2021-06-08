cc7f5e7b2596f36171fa04c5197d94c015130501
RaynCover <56882731+RaynCover@users.noreply.github.com>
2021-06-08 20:10:12 +0200

    Add files via upload

---
+++ rejestracja.php
@@ -0,0 +1,231 @@
+<?php
+
+	session_start();
+	
+	if (isset($_POST['email']))
+	{
+		$wszystko_OK=true;
+		
+		//Sprawdź poprawność loginu
+		$nick = $_POST['nick'];
+		
+		//Sprawdzenie długości loginu (3-26 znaków)
+		if ((strlen($nick)<3) || (strlen($nick)>26))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_nick']="Login musi posiadać od 3 do 26 znaków!";
+		}
+		
+		if (ctype_alnum($nick)==false)
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_nick']="Login może składać się tylko z liter i cyfr (bez polskich znaków)<br>Nie mniej niż 3 i nie więcej niż 26 znaków!";
+		}
+		
+		// Sprawdź poprawność adresu email
+		$email = $_POST['email'];
+		$emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+		
+		if ((filter_var($emailB, FILTER_VALIDATE_EMAIL)==false) || ($emailB!=$email))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_email']="Podaj poprawny adres e-mail!";
+		}
+		
+		//Sprawdź poprawność hasła
+		$haslo1 = $_POST['haslo1'];
+		$haslo2 = $_POST['haslo2'];
+		
+		if ((strlen($haslo1)<3) || (strlen($haslo1)>32))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_haslo']="Hasło musi posiadać od 3 do 32 znaków!";
+		}
+		
+		if ($haslo1!=$haslo2)
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_haslo']="Podane hasła nie są identyczne!";
+		}	
+
+		
+		
+		//Czy zaakceptowano regulamin?
+		if (!isset($_POST['regulamin']))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_regulamin']="Potwierdź akceptację regulaminu!";
+		}				
+		
+		
+		
+		//Zapamiętaj wprowadzone dane
+		$_SESSION['fr_nick'] = $nick;
+		$_SESSION['fr_email'] = $email;
+		$_SESSION['fr_haslo1'] = $haslo1;
+		$_SESSION['fr_haslo2'] = $haslo2;
+		if (isset($_POST['regulamin'])) $_SESSION['fr_regulamin'] = true;
+		
+		require_once "connect.php";
+		mysqli_report(MYSQLI_REPORT_STRICT);
+		
+		try 
+		{
+			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+			if ($polaczenie->connect_errno!=0)
+			{
+				throw new Exception(mysqli_connect_errno());
+			}
+			else
+			{
+				//Czy email już istnieje?
+				$rezultat = $polaczenie->query("SELECT Id_uzytkownika FROM uzytkownicy_io WHERE Email='$email'");
+				
+				if (!$rezultat) throw new Exception($polaczenie->error);
+				
+				$ile_takich_maili = $rezultat->num_rows;
+				if($ile_takich_maili>0)
+				{
+					$wszystko_OK=false;
+					$_SESSION['e_email']="Istnieje już konto przypisane do tego adresu e-mail!";
+				}		
+
+				//Czy login jest już zajęty?
+				$rezultat = $polaczenie->query("SELECT Id_uzytkownika FROM uzytkownicy_io WHERE Login='$nick'");
+				
+				if (!$rezultat) throw new Exception($polaczenie->error);
+				
+				$ile_takich_nickow = $rezultat->num_rows;
+				if($ile_takich_nickow>0)
+				{
+					$wszystko_OK=false;
+					$_SESSION['e_nick']="Istnieje już gracz o takim nicku! Wybierz inny.";
+				}
+				
+				if ($wszystko_OK==true)
+				{
+					//Wszystkie testy zaliczone, dodajemy użytkownika do bazy
+					
+					if ($polaczenie->query("INSERT INTO uzytkownicy_io VALUES (NULL, '$nick', '$haslo1', '$email')"))
+					{
+						$_SESSION['udanarejestracja']=true;
+						header('Location: zarejestrowany.php');
+					}
+					else
+					{
+						throw new Exception($polaczenie->error);
+					}
+					
+				}
+				
+				$polaczenie->close();
+			}
+			
+		}
+		catch(Exception $e)
+		{
+			echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
+			echo '<br />Informacja developerska: '.$e;
+		}
+		
+	}
+	
+	
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Astagram</title>
+
+</head>
+
+<body>
+
+	<form method="post">
+	
+		Login: <br /> <input type="text" value="<?php
+			if (isset($_SESSION['fr_nick']))
+			{
+				echo $_SESSION['fr_nick'];
+				unset($_SESSION['fr_nick']);
+			}
+		?>" name="nick" /><br />
+		
+		<?php
+			if (isset($_SESSION['e_nick']))
+			{
+				echo '<div class="error">'.$_SESSION['e_nick'].'</div>';
+				unset($_SESSION['e_nick']);
+			}
+		?>
+		
+		E-mail: <br /> <input type="text" value="<?php
+			if (isset($_SESSION['fr_email']))
+			{
+				echo $_SESSION['fr_email'];
+				unset($_SESSION['fr_email']);
+			}
+		?>" name="email" /><br />
+		
+		<?php
+			if (isset($_SESSION['e_email']))
+			{
+				echo '<div class="error">'.$_SESSION['e_email'].'</div>';
+				unset($_SESSION['e_email']);
+			}
+		?>
+		
+		Twoje hasło: <br /> <input type="password"  value="<?php
+			if (isset($_SESSION['fr_haslo1']))
+			{
+				echo $_SESSION['fr_haslo1'];
+				unset($_SESSION['fr_haslo1']);
+			}
+		?>" name="haslo1" /><br />
+		
+		<?php
+			if (isset($_SESSION['e_haslo']))
+			{
+				echo '<div class="error">'.$_SESSION['e_haslo'].'</div>';
+				unset($_SESSION['e_haslo']);
+			}
+		?>		
+		
+		Powtórz hasło: <br /> <input type="password" value="<?php
+			if (isset($_SESSION['fr_haslo2']))
+			{
+				echo $_SESSION['fr_haslo2'];
+				unset($_SESSION['fr_haslo2']);
+			}
+		?>" name="haslo2" /><br />
+		
+		<label>
+			<input type="checkbox" name="regulamin" <?php
+			if (isset($_SESSION['fr_regulamin']))
+			{
+				echo "checked";
+				unset($_SESSION['fr_regulamin']);
+			}
+				?>/> Akceptuję <a href='regulamin.txt' target='_blanked'>regulamin</a>
+		</label>
+		
+		<?php
+			if (isset($_SESSION['e_regulamin']))
+			{
+				echo '<div class="error">'.$_SESSION['e_regulamin'].'</div>';
+				unset($_SESSION['e_regulamin']);
+			}
+		?>	
+		<br><br>
+
+		<input type="submit" value="Zarejestruj się" /><br>
+		
+	</form>
+Posiadasz już konto? <a href='zaloguj.php' style='color: red;'>Zaloguj się tutaj!</a><br>
+
+
+</body>
+</html>
---
+++ zarejestrowany.php
@@ -0,0 +1,57 @@
+<?php
+
+	session_start();
+	
+	if (!isset($_SESSION['udanarejestracja']))
+	{
+		header('Location: index.php');
+		exit();
+	}
+	else
+	{
+		unset($_SESSION['udanarejestracja']);
+	}
+	
+	//Usuwanie zmiennych pamiętających wartości wpisane do formularza
+	if (isset($_SESSION['fr_nick'])) unset($_SESSION['fr_nick']);
+	if (isset($_SESSION['fr_email'])) unset($_SESSION['fr_email']);
+	if (isset($_SESSION['fr_haslo1'])) unset($_SESSION['fr_haslo1']);
+	if (isset($_SESSION['fr_haslo2'])) unset($_SESSION['fr_haslo2']);
+	if (isset($_SESSION['fr_regulamin'])) unset($_SESSION['fr_regulamin']);
+	
+	//Usuwanie błędów rejestracji
+	if (isset($_SESSION['e_nick'])) unset($_SESSION['e_nick']);
+	if (isset($_SESSION['e_email'])) unset($_SESSION['e_email']);
+	if (isset($_SESSION['e_haslo'])) unset($_SESSION['e_haslo']);
+	if (isset($_SESSION['e_regulamin'])) unset($_SESSION['e_regulamin']);
+	if (isset($_SESSION['e_bot'])) unset($_SESSION['e_bot']);
+	
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Astagram</title>
+</head>
+
+<body>
+
+<a href="rejestracja.php">ZAREJESTRUJ SIĘ!</a>
+	<br /><br />
+	TERAZ MOŻESZ SIĘ ZALOGOWAĆ!
+	<form action="zaloguj.php" method="post">
+	
+		Login: <br /> <input type="text" name="login" /> <br />
+		Hasło: <br /> <input type="password" name="haslo" /> <br /><br />
+		<input type="submit" value="Zaloguj się" />
+	
+	</form>
+	
+<?php
+	if(isset($_SESSION['blad']))	echo $_SESSION['blad'];
+?>
+
+</body>
+</html>
